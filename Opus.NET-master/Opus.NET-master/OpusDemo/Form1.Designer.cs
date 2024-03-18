4f81b4f1e32b5baef4ec497bdc612dbc454efc16
malkamius <cking999@gmail.com>
2024-03-17 23:33:03 -0500

    3rd party libraries

---
+++ Opus.NET-master/Opus.NET-master/OpusDemo/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace OpusDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            comboBox1 = new System.Windows.Forms.ComboBox();
+            comboBox2 = new System.Windows.Forms.ComboBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(113, 149);
+            button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(88, 27);
+            button1.TabIndex = 0;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Enabled = false;
+            button2.Location = new System.Drawing.Point(217, 149);
+            button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(88, 27);
+            button2.TabIndex = 1;
+            button2.Text = "Stop";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new System.Drawing.Point(113, 85);
+            comboBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new System.Drawing.Size(191, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new System.Drawing.Point(113, 118);
+            comboBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new System.Drawing.Size(191, 23);
+            comboBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.Location = new System.Drawing.Point(14, 10);
+            label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(290, 72);
+            label1.TabIndex = 4;
+            label1.Text = "Select an input and output device. Once you click \"Start\" audio will be captured from your micrphone, encoded and decoded using Opus and played through the output device.";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(14, 89);
+            label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(76, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Input Device:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(14, 121);
+            label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(86, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Output Device:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(318, 187);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Opus Demo";
+            Load += Form1_Load;
+            Click += Form1_Click;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Opus.NET-master/Opus.NET-master/OpusDemo/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NAudio.Wave;
+using FragLabs.Audio.Codecs;
+using System.Diagnostics;
+
+namespace OpusDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            {
+                comboBox1.Items.Add(WaveIn.GetCapabilities(i).ProductName);
+            }
+            if (WaveIn.DeviceCount > 0)
+                comboBox1.SelectedIndex = 0;
+            for (int i = 0; i < WaveOut.DeviceCount; i++)
+            {
+                comboBox2.Items.Add(WaveOut.GetCapabilities(i).ProductName);
+            }
+            if (WaveOut.DeviceCount > 0)
+                comboBox2.SelectedIndex = 0;
+
+            var data = System.IO.File.ReadAllBytes("I:\\audiomux\\opus.bin");
+            data = data.Skip(4).ToArray();
+            var decoder = OpusDecoder.Create(48000, 1);
+            decoder.MaxDataBytes = 192000;
+            _playBuffer = new BufferedWaveProvider(new WaveFormat(48000, 32, 1));
+            using (_waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+            {
+                _waveOut.DeviceNumber = comboBox2.SelectedIndex;
+                _waveOut.Init(_playBuffer);
+
+                //while (data.Length > 0)
+                {
+                    try
+                    {
+                        // Call opus decode on the Opus stream parsed from the webm container
+                        // decodedLength is the framecount
+                        var decoded = decoder.Decode(data, data.Length, out var decodedLength);
+
+                        //_playBuffer.AddSamples(decoded, 0, decodedLength);
+                        System.Diagnostics.Debug.WriteLine(Convert.ToBase64String(decoded, 0, decodedLength));
+
+
+                    }
+                    //break;
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.ToString());
+                        //data = data.Skip(1).ToArray();
+                    }
+                }
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button1.Enabled = false;
+            StartEncoding();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            StopEncoding();
+        }
+
+        WaveIn _waveIn;
+        WaveOut _waveOut;
+        BufferedWaveProvider _playBuffer;
+        OpusEncoder _encoder;
+        OpusDecoder _decoder;
+        int _segmentFrames;
+        int _bytesPerSegment;
+        ulong _bytesSent;
+        DateTime _startTime;
+        Timer _timer = null;
+
+        void StartEncoding()
+        {
+            _startTime = DateTime.Now;
+            _bytesSent = 0;
+            _segmentFrames = 960;
+            _encoder = OpusEncoder.Create(48000, 1, FragLabs.Audio.Codecs.Opus.Application.Voip);
+            _encoder.Bitrate = 8192;
+            _decoder = OpusDecoder.Create(48000, 1);
+            _bytesPerSegment = _encoder.FrameByteCount(_segmentFrames);
+
+            _waveIn = new WaveIn(WaveCallbackInfo.FunctionCallback());
+            _waveIn.BufferMilliseconds = 50;
+            _waveIn.DeviceNumber = comboBox1.SelectedIndex;
+            _waveIn.DataAvailable += _waveIn_DataAvailable;
+            _waveIn.WaveFormat = new WaveFormat(48000, 16, 1);
+
+            _playBuffer = new BufferedWaveProvider(new WaveFormat(48000, 16, 1));
+
+            _waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback());
+            _waveOut.DeviceNumber = comboBox2.SelectedIndex;
+            _waveOut.Init(_playBuffer);
+
+            _waveOut.Play();
+            _waveIn.StartRecording();
+
+            if (_timer == null)
+            {
+                _timer = new Timer();
+                _timer.Interval = 1000;
+                _timer.Tick += _timer_Tick;
+            }
+            _timer.Start();
+        }
+
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            var timeDiff = DateTime.Now - _startTime;
+            var bytesPerSecond = _bytesSent / timeDiff.TotalSeconds;
+            Console.WriteLine("{0} Bps", bytesPerSecond);
+        }
+
+        byte[] _notEncodedBuffer = new byte[0];
+        void _waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] soundBuffer = new byte[e.BytesRecorded + _notEncodedBuffer.Length];
+            for (int i = 0; i < _notEncodedBuffer.Length; i++)
+                soundBuffer[i] = _notEncodedBuffer[i];
+            for (int i = 0; i < e.BytesRecorded; i++)
+                soundBuffer[i + _notEncodedBuffer.Length] = e.Buffer[i];
+
+            int byteCap = _bytesPerSegment;
+            int segmentCount = (int)Math.Floor((decimal)soundBuffer.Length / byteCap);
+            int segmentsEnd = segmentCount * byteCap;
+            int notEncodedCount = soundBuffer.Length - segmentsEnd;
+            _notEncodedBuffer = new byte[notEncodedCount];
+            for (int i = 0; i < notEncodedCount; i++)
+            {
+                _notEncodedBuffer[i] = soundBuffer[segmentsEnd + i];
+            }
+
+            for (int i = 0; i < segmentCount; i++)
+            {
+                byte[] segment = new byte[byteCap];
+                for (int j = 0; j < segment.Length; j++)
+                    segment[j] = soundBuffer[(i * byteCap) + j];
+                int len;
+                byte[] buff = _encoder.Encode(segment, segment.Length, out len);
+                _bytesSent += (ulong)len;
+                buff = _decoder.Decode(buff, len, out len);
+                _playBuffer.AddSamples(buff, 0, len);
+                System.Diagnostics.Debug.WriteLine("Buffer");
+                System.Diagnostics.Debug.WriteLine(Convert.ToBase64String(buff));
+                //System.Diagnostics.Debug.WriteLine(Convert.ToBase64String(buff).Substring(0, 50));
+            }
+
+        }
+
+        void StopEncoding()
+        {
+            _timer.Stop();
+            _waveIn.StopRecording();
+            _waveIn.Dispose();
+            _waveIn = null;
+            _waveOut.Stop();
+            _waveOut.Dispose();
+            _waveOut = null;
+            _playBuffer = null;
+            _encoder.Dispose();
+            _encoder = null;
+            _decoder.Dispose();
+            _decoder = null;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            _playBuffer = new BufferedWaveProvider(new WaveFormat(48000, 32, 1));
+            _waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback());
+            {
+                _waveOut.DeviceNumber = comboBox2.SelectedIndex;
+                _waveOut.Init(_playBuffer);
+                var transformed = (from c in new string((char)255, 960000) select (byte)c).ToArray();
+                transformed = Convert.FromBase64String("//8AAAAAAQACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAEAAQABAAEAAgADAAMABAAFAAUABQAEAAQAAwADAAIAAgABAAEAAQABAAIAAgACAAIAAgACAAIAAgACAAEAAQABAAIAAgACAAMAAwADAAMAAwADAAMAAwADAAIAAgACAAIAAgACAAMAAwADAAMAAwADAAMAAwADAAMAAwADAAIAAgACAAIAAgACAAMAAwADAAMAAwADAAIAAgACAAEAAQABAAAAAAAAAAAAAAAAAAEAAQABAAEAAQABAAEAAQABAAIAAgADAAMAAwADAAIAAgACAAEAAQABAAEAAQABAAIAAgADAAMAAwADAAIAAgACAAIAAgACAAMAAwADAAQABAAEAAQABAAEAAQABAAEAAQABAAEAAMAAwADAAMAAwACAAIAAQAAAAAAAAAAAAAAAAAAAAEAAQACAAIAAwAEAAQABAAEAAQABAAEAAMAAwADAAMAAwADAAMAAwACAAIAAQAAAAAAAAAAAAAAAAAAAAEAAQABAAEAAQABAAEAAQABAAIAAgADAAQABAAEAAQABAAEAAMAAwADAAMAAwADAAQABAAEAAQABAAEAAMAAwADAAIAAgACAAIAAgADAAMABAAEAAQABAAEAAMAAwADAAIAAgABAAEAAQABAAIAAgADAAMAAwADAAMAAwADAAQABAAEAAQABAAEAAMAAwADAAIAAgACAAEAAQABAAEAAQABAAIAAgACAAMAAwAEAAQABAAEAAMAAwADAAIAAgACAAIAAgACAAIAAgACAAMAAwADAAQABAAEAAQABAAEAAQABAAEAAQABAAEAAMAAwADAAMAAwADAAIAAgACAAIAAgACAAEAAQABAAEAAQABAAIAAgACAAMAAwADAAMAAwADAAMAAwADAAMAAwADAAIAAgACAAEAAQABAAEAAQABAAEAAQABAAAAAAAAAAAAAAAAAAEAAQABAAEAAQABAAAAAAAAAP///////////////////////wAAAAAAAAAAAAAAAP///////////////wAAAAAAAAEAAQABAAEAAQABAAEAAQABAAEAAQABAAAAAAAAAAAAAAAAAAAAAAAAAP//////////AAABAAEAAgADAAMAAwADAAIAAgABAAEAAAD//////////wAAAAAAAAEAAQABAAEAAQABAAAAAAAAAP////////////////////////7//v/+/////////wAAAAAAAAEAAQABAAEAAQABAAAAAAAAAP////////7//v/+//7//v/+//7//v/+//7//v/+//7//v/+//3//f/9//3//f/9//7//v/+//////////////////////////7//v/+//7//v/+//////////////////////////7//v/9//z//P/8//z//P/8//3//f/9//7//v/+//7//v/+//3//f/9//z//P/8//z//P/8//3//f/9//3//f/9//3//f/9//7//v/+//7//v/+//3//f/8//v/+//7//z//P/8//3//f/+//7//v/+//3//f/9//z//P/8//z//P/8//z//P/8//3//f/+//7///8AAAAAAAAAAP/////+//7//f/8//z//P/8//3//f/9//3//f/9//3//f/9//z//P/8//3//f/9//7//v/+//7//v/+//3//f/9//3//f/9//7//v/+//7//v/+//3//f/9//z//P/8//z//P/9//3//v////////////7//v/+//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//z//P/8//3//f/9//3//f/9//7//v/+//7//v/9//3//P/8//z//P/8//z//P/8//3//f/9//3//f/9//z//P/8//v/+//7//v/+//7//z//P/8//z//P/8//z//P/8//3//f/9//7//v/+//7//v/9//3//P/7//v/+//7//z//P/8//3//f/9//7//v/+//7//v/+//3//f/9//3//f/9//3//f/9//z//P/8//z//P/8//z//P/8//3//f/9//7//v/+//7//v/+//7//v/+//7//v/+//3//f/8//z//P/8//3//f/9//3//f/9//z//P/8//z//P/8//3//f/+//7///////////////7//v/+//7//v/+//////////////////7//v/+//7//v/+//7//v/+/////////////v/9//z//P/8//z//P/8//3//f/9//3//f/9//3//f/9//z//P/8//z//P/8//3//f/9//3//f/9//7//v/+//////////////////7//v/+//////////////////////////7//v/+//7//v/+//////////////////////////7//v/+//////////////////7//v/9//3//P/8//z//P/9//3//v///wAAAAAAAAAAAAAAAP///////////////wAAAAABAAEAAQABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==");
+                //for (var i = 0; i < 4; i++) transformed = transformed.Concat(transformed).ToArray();                
+                for(var i = 0; i < 32; i++)
+                _playBuffer.AddSamples(transformed, 0, (int)transformed.Length);
+                _waveOut.Play();
+                _waveOut.PlaybackStopped += _waveOut_PlaybackStopped;
+            }
+        }
+
+        private void _waveOut_PlaybackStopped(object sender, EventArgs e)
+        {
+            _waveOut.Dispose();
+        }
+    }
+}
